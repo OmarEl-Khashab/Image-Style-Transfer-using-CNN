dd8532947795e1f898643d639e929e4d9b900749
Omar El-Khashab <60649083+OmarEl-Khashab@users.noreply.github.com>
2024-11-27 13:48:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,2 @@
-# class Solution:
-#     def reverseString(self, s):
-#         """
-#         Do not return anything, modify s in-place instead.
-#         """
-#         string_postion = len(s) - 1
-#         reverse_string = " "
-#         while string_postion >= 0:
-#             reverse_string +=s[string_postion]
-#             string_postion -= 1
 #
-#         return reverse_string
-
-class Solution:
-    def reverseString(self, s):
-        left, right = 0, len(s) - 1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left, right = left + 1, right - 1
-call = Solution()
-print(call.reverseString(["h","e","l","l","o"]))
 
